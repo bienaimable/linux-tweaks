0f3955fd5c867b7772bb3542e3e669a836953945
François Pillot <f.pillot@criteo.com>
2018-05-14 11:19:14 -0400

    Reformatting noice install

--- noice/install.sh
+++ noice/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
 apt-get install libncurses5-dev
 rm -rf /opt/noice
 git clone git://git.2f30.org/noice.git /opt/noice
-cp ../../configs/noice/config.h /opt/noice/
+cp config.h /opt/noice/
 cd /opt/noice
 make
 ln -rsf ./noice /usr/bin/
+echo "h quit" |lesskey - 
