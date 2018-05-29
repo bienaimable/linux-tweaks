eab83d6f64be40fed1b8eeb11e31399114d7b698
François Pillot <f.pillot@criteo.com>
2018-05-29 16:04:12 -0400

    Splitting git config for high and low resolution

--- vim/install.sh
+++ vim/install.sh
@@ -5,7 +5,17 @@ if [[ $EUID -ne 0 ]]; then
    echo "Setting up .vimrc for regular user" 
    sleep 1
    mkdir -p ~/.vim &
-   ln -rsf .vimrc ~/.vimrc
+   #ln -rsf .vimrc ~/.vimrc
+   IN=$(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/')
+   arrIN=(${IN//x/ })
+   if [[ $arrIN < 1400 ]] ; 
+   then 
+       echo Using low resolution configuration;
+       cp .vimrc.low_res ~/.vimrc
+   else
+       echo Using high resolution configuration;
+       cp .vimrc.high_res ~/.vimrc
+   fi;
    echo "Make sure to also run this script as root" 
    sleep 1
    exit 
